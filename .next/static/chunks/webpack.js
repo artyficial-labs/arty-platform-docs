9743db9085ca17aeb3f5cd2da2e4933477f1a199
Tukisa <tukisa@artyficiallabs.com>
2024-03-26 22:36:56 +0200

    Extra updates

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2bf8a01208c95a90"; }
+/******/ 		__webpack_require__.h = function() { return "a7b930416bc26d1a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
